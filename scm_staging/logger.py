f66c5eb8c30f609257f391f876ddd5aa00a5b6b3
AvengerMoJo <AvengerMoJo@users.noreply.github.com>
2023-06-05 14:05:36 +0800

    Adding logging color for different level (#20)
    Signed-off-by: Alex Lau (AvengerMoJo) <alau@suse.com>

--- scm_staging/logger.py
+++ scm_staging/logger.py
@@ -1,7 +1,31 @@
 import logging
 
+
+class CustomFormatter(logging.Formatter):
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    log_format = (
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    )
+    FORMATS = {
+        logging.DEBUG: grey + log_format + reset,
+        logging.INFO: grey + log_format + reset,
+        logging.WARNING: yellow + log_format + reset,
+        logging.ERROR: red + log_format + reset,
+        logging.CRITICAL: bold_red + log_format + reset,
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
 handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter(fmt="%(levelname)s: %(message)s"))
+handler.setFormatter(CustomFormatter())
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
